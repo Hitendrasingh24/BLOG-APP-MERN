6e2f88670c06b47070fc734552b9947f9926e232
Hitendrasingh24 <hitendrasingh511@gmail.com>
2024-02-04 13:45:14 +0530

    just updated some things

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log("DataBase connected")
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
