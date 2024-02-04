904d4872b1f5e0061cedf1d7b7244b1eac47062d
Hitendrasingh24 <hitendrasingh511@gmail.com>
2024-02-04 13:11:04 +0530

    completed header component and moving towards sign up backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express();
+const dotenv =require('dotenv');
+const mongoose =require('mongoose')
+
+dotenv.config();
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("DataBase connected")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.get('/get',(req,res)=>{
+    res.send("hi")
+})
+
 
 app.listen(5000,()=>{
 console.log("sever started")
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,11 +1,12 @@
-import { Navbar, TextInput } from 'flowbite-react'
+import { Button, Navbar, TextInput, theme } from 'flowbite-react'
 import React from 'react'
 import {AiOutlineSearch} from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import {FaSun,FaMoon} from 'react-icons/fa'
+import { Link, useLocation } from 'react-router-dom'
+// import (toggleTheme) from 
 export default function Header() {
-  return (
-   
-   
+  const path = useLocation().pathname
+  return ( 
    <Navbar className='border-b-2'>
     <Link to ='/'
       className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -18,10 +19,43 @@ export default function Header() {
             <TextInput type ="text"
             placeholder='search here'
             rightIcon={AiOutlineSearch}
-            >
-
-            </TextInput>
+            className='hidden lg:inline'
+            />
         </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill> 
+        <AiOutlineSearch></AiOutlineSearch>
+        </Button>
+        <div className='flex gap-2 md:order-2' >
+        <Button className='w-12 h-10 hidden sm:inline'
+        color='gray'
+        pill
+        >
+          <FaMoon/>
+        </Button>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue'>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle></Navbar.Toggle>
+       </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path==='/'} as={'div'}>
+              <Link to='/'>
+                Home
+              </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path==='/about'}as={'div'}>
+              <Link to='/about'>
+                About
+              </Link>
+          </Navbar.Link>
+          <Navbar.Link active ={path ==='/projects'}as={'div'}>
+              <Link to='/projects'>
+                Projects
+              </Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
     )
 }
