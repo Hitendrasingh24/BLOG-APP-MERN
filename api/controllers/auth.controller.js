fccc4524213e4e8ff87ddedfed833b958e3a465d
Hitendrasingh24 <hitendrasingh511@gmail.com>
2024-02-08 01:42:26 +0530

    CREATED SIGN UP ROUTES AND ERROR HANDLING MIDDLEWARES

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,33 @@
-export const signup =async(req,res)=>{
-    console.log(req.body);
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+// import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res,next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400,'All fields are required'));
+  }
+
+const hashedPassword=bcryptjs.hashSync(password,10);
+  const newUser = new User({
+    username,
+    email,
+    password:hashedPassword,
+  });
+try {
+    await newUser.save();
+    res.json('Signup successful');
 }
+catch(error){
+    next(error);
+}
+};
